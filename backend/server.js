cbcc6a89229668e00b94e1f07e7783f2e652e402
sharan.medamoni4243@gmail.com <sharan.medamoni4243@gmail.com>
2025-08-29 23:58:09 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ const corsOptions = {
     'http://localhost:3000', 
     'http://localhost:5173', 
     'http://localhost:5174',
-    'https://dvintage-spices.netlify.app'
+    'https://dvintage-spices.netlify.app',
+    'https://admin-dvintage.netlify.app/'
   ], // Allow both localhost and deployed frontend URLs
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
